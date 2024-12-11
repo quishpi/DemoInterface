fc919ef55dce08ef79b45fee5949155b4c9574f3
Luis Quishpi <luis.quishpi@outlook.com>
2024-12-11 16:54:27 -0500

    Update: Rectangle class added.

--- src/ec/edu/puce/interfaces/Main.java
+++ src/ec/edu/puce/interfaces/Main.java
@@ -5,6 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hola PUCE");
 
+		Rectangulo rectangulo1=new Rectangulo();
+		rectangulo1.setAltura(25);
+		rectangulo1.setBase(14);
+		System.out.println("Área del rectángulo: " + rectangulo1.calcularArea());
 	}
 
 }
